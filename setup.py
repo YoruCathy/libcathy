21c1fefc05a280267f9ae3677cb489f05b402b3f
YoruCathy <cathyye0809@gmail.com>
2023-05-30 13:33:17 -0400

    new files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="libcathy",
+    version="0.0.1",
+    author="Cathy Ye",
+    author_email="cathyye0809@gmail.com",
+    python_requires=">=3.8.0"
+)
